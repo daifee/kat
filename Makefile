277e3a6763d3b179621de340dc3b50d9f4462e73
daifee <mr_zhangjiayong@163.com>
2022-03-28 00:05:13 +0800

    make

--- Makefile
+++ Makefile
@@ -1,16 +1,58 @@
 
 
-TSC := ./node_modules/.bin/tsc
-NODE := node
+SOURCES = packages
 
-# 清除文件
-clean-lib:
-	rm -rf ./lib
+NODE := node
+BABEL := node_modules/.bin/babel
+NPM := npm
+
+
+bootstrap: clean
+	$(NPM) install
+	$(NODE) scripts/generators/tsconfig.pkg.js
+
+
+build:
+	echo "TODO: make build"
+
+watch:
+	echo "TODO: watch"
+
+
+# 安装的依赖
+# 编译生成的目标文件
+clean:
+	$(MAKE) clean-lib-all
+	$(MAKE) clean-dependencies-all
+
+# 清除生成的文件
+clean-lib-all:
+	$(foreach source, \
+		$(SOURCES), \
+		rm -rf $(source)/*/lib \
+	)
+
+
+clean-dependencies-all:
+	rm -rf node_modules
+	rm -rf package-lock.json
+	$(foreach source, \
+		$(SOURCES), \
+		$(call clean-dependencies, $(source)) \
+	)
+
 
 # 只构建，不打包
-build-no-bundle: clean-lib
-	$(TSC)
+build-no-bundle:
+	$(BABEL) --relative packages/*/src --extensions ".ts" -d ../lib --watch
 
 
 test:
-	$(NODE) ./packages/stack/lib/index.js
+	$(NODE) packages/stack/lib/index.js
+
+
+
+define clean-dependencies
+	rm -rf $(1)/*/node_modules
+	rm -rf $(1)/*/package-lock.json
+endef
---
+++ scripts/generators/tsconfig.pkg.js
@@ -0,0 +1,66 @@
+/**
+ * 生成 `./tsconfig.pkg.json` 文件
+ */
+const fs = require('fs');
+const path = require('path');
+
+const root = process.cwd();
+
+/**
+ * name 包名 `@daifee/name`
+ * path 绝对路径 `/path/to/name`
+ * relativePath 相对路径 `./packages/name`
+ */
+function getPackages(subRoot) {
+  return fs.readdirSync(path.join(root, subRoot))
+    .map((dir) => {
+      return {
+        name: `@daifee/${dir}`,
+        path: path.resolve(root, subRoot, dir),
+        relativePath: `./${subRoot}/${dir}`
+      };
+    })
+    // 过滤条件1
+    .filter((item) => {
+      let filteredMsg = '';
+
+      try {
+        const metaFile = path.resolve(item.path, 'package.json');
+        const meta = require(metaFile);
+
+        if (meta.name !== item.name) {
+          filteredMsg = `package.name !== "@daifee/\${dirname}": ${item.relativePath}`;
+        }
+      } catch (err) {
+        filteredMsg = `缺少文件：${metaFile}`;
+      }
+      // log
+      console.warn(filteredMsg);
+      return !filteredMsg;
+    })
+    // 过滤条件2
+    .filter((item) => {
+      const indexFile = path.resolve(item.path, 'src/index.ts');
+      return fs.existsSync(indexFile);
+    });
+}
+
+const packagesRoot = [
+  ...getPackages('packages')
+];
+
+const config = {
+  include: packagesRoot.map((pkg) => {
+    return `${pkg.relativePath}/src/**/*.ts`
+  }),
+  compilerOptions: {
+    paths: Object.fromEntries(packagesRoot.map((pkg) => {
+      return [pkg.name, [`${pkg.relativePath}/src`]]
+    }))
+  }
+};
+
+fs.writeFileSync(
+  path.resolve(root, 'tsconfig.pkg.json'),
+  JSON.stringify(config, null, 2)
+);
