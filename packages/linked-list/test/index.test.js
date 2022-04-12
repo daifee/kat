8e95cef098143d180ca68b73b6914b0dba854f12
daifee <mr_zhangjiayong@163.com>
2022-04-13 02:57:15 +0800

    te

--- packages/linked-list/test/index.test.js
+++ packages/linked-list/test/index.test.js
@@ -1,5 +1,5 @@
 
-import { describe, expect, test } from '@jest/globals';
+// import { describe, expect, test } from '@jest/globals';
 import LinkedList from '../lib';
 
 function runCases(cases = []) {
