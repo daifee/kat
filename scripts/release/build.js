d532c4c1c481bc4fe230735afcd969b79f620ead
daifee <mr_zhangjiayong@163.com>
2022-04-22 04:50:04 +0800

    0.3.5

---
+++ scripts/release/build.js
@@ -0,0 +1,28 @@
+
+
+const { execFileSync } = require('child_process');
+const cliOptions = require('./cli-options');
+const { getPackage } = require('./workspaces');
+const { extractPackageNameAndVersion } = require('./utils');
+
+const opts = cliOptions();
+const meta = extractPackageNameAndVersion(opts['release-tag']);
+
+const workspace = getPackage(opts.root, meta.name);
+
+
+if (!workspace) {
+  throw new Error(`不存在 package: ${meta['release-tag']}`);
+}
+
+const stdout = execFileSync('./scripts/build.sh', {
+  cwd: process.cwd(),
+  env: {
+    ...process.env,
+    PACKAGE: workspace.path.replace(/^packages\//, ''),
+  },
+  encoding: 'utf-8',
+});
+
+
+console.log(stdout);
